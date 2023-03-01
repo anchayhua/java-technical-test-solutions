24fa41c2dc3149cfd0054fb242a883a3145ffb74
Nicanor <nanchayhua@vivienda.gob.pe>
2023-03-01 15:13:23 -0500

    ok

--- src/test/java/com/quique/api/ApplicationTests.java
+++ src/test/java/com/quique/api/ApplicationTests.java
@@ -1,85 +1,51 @@
-//package com.quique.api;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import com.quique.api.dto.PlanetDto;
-//import org.junit.jupiter.api.*;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.web.client.TestRestTemplate;
-//import org.springframework.http.HttpHeaders;
-//import org.springframework.test.context.junit4.SpringRunner;
-//import org.springframework.test.web.servlet.MockMvc;
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-////@TestPropertySource(locations = "classpath:application-test.properties")
-//@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-//@AutoConfigureMockMvc
-//public class ApplicationTests {
-//
-//    static HttpHeaders authHeaders = new HttpHeaders();
-//    ObjectMapper om = new ObjectMapper();
-//
-//    @Autowired
-//    private MockMvc mockMvc;
-//
-//    @Autowired
-//    private TestRestTemplate restTemplate;
-//
-////    @Test
-////    public void testGetPlanets() {
-////        
-////        HttpHeaders headers = new HttpHeaders();
-//////headers.set("Authorization", "Bearer " + accessToken);
-////
-////        HttpEntity<String> entity = new HttpEntity<>(headers);
-////
-////        ResponseEntity<List<PlanetDto>> response = restTemplate.exchange("/api/planets?param={param}&page={page}",
-////                HttpMethod.GET, entity, new ParameterizedTypeReference<List<PlanetDto>>() {
-////                }, "paramValue", "pageValue");
-//////        ResponseEntity<List<PlanetDto>> response = restTemplate.exchange("/api/planets?param={param}&page={page}",
-//////                HttpMethod.GET, null, new ParameterizedTypeReference<List<PlanetDto>>() {
-//////                }, "paramValue", "pageValue");
-////
-////        assertEquals(HttpStatus.OK, response.getStatusCode());
-////        assertNotNull(response.getBody());
-////    }
-//    @Test
-//    @Order(1)
-//    @DisplayName("status200WhenValidRequest")
-//    public void status200WhenValidRequest() throws Exception {
-//        PlanetDto expectedRecord = PlanetDto.builder()
-//                .area(13.2f)
-//                .price(23.0f)
-//                .city("Delhi")
-//                .build();
-//        PlanetDto actualHome = om.readValue(mockMvc.perform(post("/api/home")
-//                .contentType("application/json")
-//                .headers(authHeaders)
-//                .content(om.writeValueAsString(expectedRecord)))
-//                .andDo(print())
-//                .andExpect(status().isCreated()).andReturn().getResponse().getContentAsString(), Home.class);
-//
-//        Assertions.assertEquals(expectedRecord.getArea(), actualHome.getArea());
-//        Assertions.assertEquals(expectedRecord.getPrice(), actualHome.getPrice());
-//        Assertions.assertEquals(expectedRecord.getCity(), actualHome.getCity());
-//    }
-//
-////    @Test
-////    public void testPostPeople() {
-////        PeopleDto peopleDto = new PeopleDto();
-////        // Set properties of PeopleDto
-////
-////        HttpEntity<PeopleDto> request = new HttpEntity<>(peopleDto);
-////
-////        ResponseEntity<List<PeopleDto>> response = restTemplate.exchange("/api/peoples",
-////                HttpMethod.POST, request, new ParameterizedTypeReference<List<PeopleDto>>() {
-////                });
-////
-////        assertEquals(HttpStatus.OK, response.getStatusCode());
-////        assertNotNull(response.getBody());
-////    }
-//}
+package com.quique.api;
+
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class ApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @Order(1)
+    @DisplayName("status200WhenValidRequest")
+    public void getAllEmployeesAPI() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/api/planets")
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[*]").isNotEmpty())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[*].name").exists());
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("status200WhenGetIdPlanet")
+    public void getPlanetByIdAPI() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/api/planet/{id}", 1)
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[*]").isNotEmpty())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[*].name").exists());
+    }
+}
